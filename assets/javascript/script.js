edf294e8b84154d49af458d6688306f0c103f57d
ravitz <adam.ravitz@gmail.com>
2019-04-06 18:02:50 -0400

    april 6.11

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -22,49 +22,46 @@ $(document).ready(function () {
 
   $(function () {
 
-    // Get the form.
-    var form = $('#ajax-contact');
+    // init the validator
+    // validator files are included in the download package
+    // otherwise download from http://1000hz.github.io/bootstrap-validator
 
-    // Get the messages div.
-    var formMessages = $('#form-messages');
+    $('#contact-form').validator();
 
-    // Set up an event listener for the contact form.
-    $(form).submit(function (e) {
-      // Stop the browser from submitting the form.
-      e.preventDefault();
 
-      // Serialize the form data.
-      var formData = $(form).serialize();
+    // when the form is submitted
+    $('#contact-form').on('submit', function (e) {
 
-      // Submit the form using AJAX.
-      $.ajax({
-        type: 'POST',
-        url: $(form).attr('action'),
-        data: formData
-      })
-        .done(function (response) {
-          // Make sure that the formMessages div has the 'success' class.
-          $(formMessages).removeClass('error');
-          $(formMessages).addClass('success');
+        // if the validator does not prevent form submit
+        if (!e.isDefaultPrevented()) {
+            var url = "sendmail.php";
 
-          // Set the message text.
-          $(formMessages).text(response);
+            // POST values in the background the the script URL
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    // data = JSON object that contact.php returns
 
-          // Clear the form.
-          $('#name, #email, #message').val('');
-        })
-        .fail(function (data) {
-          // Make sure that the formMessages div has the 'error' class.
-          $(formMessages).removeClass('success');
-          $(formMessages).addClass('error');
+                    // we recieve the type of the message: success x danger and apply it to the 
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
 
-          // Set the message text.
-          if (data.responseText !== '') {
-            $(formMessages).text(data.responseText);
-          } else {
-            $(formMessages).text('Oops! An error occured and your message could not be sent.');
-          }
-        });
-    });
-  });
+                    // let's compose Bootstrap alert box HTML
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    
+                    // If we have messageAlert and messageText
+                    if (messageAlert && messageText) {
+                        // inject the alert to .messages div in our form
+                        $('#contact-form').find('.messages').html(alertBox);
+                        // empty the form
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
 });
