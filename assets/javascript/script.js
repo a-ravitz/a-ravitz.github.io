20bb2aa01fdbcd0b314762422bcd93d206fecebe
ravitz <adam.ravitz@gmail.com>
2019-04-06 16:31:54 -0400

    april 6.1

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-console.log("loaded")
+$(document).ready(function () {
+  console.log("loaded")
 
-  $('.slideSection').click(function(e){
-    
+  $('.slideSection').click(function (e) {
+
     var linkHref = $(this).attr('href');
     console.log($(linkHref).offset().top)
     console.log(linkHref);
@@ -10,8 +10,37 @@ console.log("loaded")
     $('html, body').animate({
       scrollTop: $(linkHref).offset().top
     }, 850);
-    
-  e.preventDefault();
+
+    e.preventDefault();
   })
 
+  if ("data-collapse" === true) {
+    $(".dropper").css("display", "none")
+  }
+
+
+  var form = $('#form'); //form id here
+  form.submit(function (event) {
+    event.preventDefault();
+    var form_status = $('<div class="form_status"></div>');
+    var $form = $(this);
+    $.ajax({
+      type: 'POST',
+      url: "../php/sendmail.php",
+      data: {
+        Name: $("#name").val(),
+        Email: $("#email").val(),
+        Message: $("#message").val()
+      },
+      beforeSend: function () {
+        form.append(form_status.html('Email is sending...').fadeIn());
+      }
+    }).done(function (data) {
+      form_status.html('Email Sent!').delay(3210).fadeOut();
+    });
+    //delete messages when submit
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+  });
 });
---
+++ assets/php/sendmail.php
@@ -0,0 +1,16 @@
+<?php
+ini_set("mail.log", "/tmp/mail.log");
+ini_set("mail.add_x_header", TRUE);
+//variables
+$name = strip_tags(htmlspecialchars($_POST['Name']));
+$email_address = strip_tags(htmlspecialchars($_POST['Email']));
+$message = strip_tags(htmlspecialchars($_POST['Message']));
+   
+// Create the email and send the message
+$to = 'adam.ravitz@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from example... contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@adamravitz.com"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address"; 
+if(mail($to,$email_subject,$email_body,$headers)){ echo "Mail sent!";} else{ echo "Error, check your logs."; }
+return true;
