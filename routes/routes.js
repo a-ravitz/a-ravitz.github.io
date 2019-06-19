94107b71f513a2e71efce31f24d9fc985f4aa7bb
a-ravitz <adam.ravitz@gmail.com>
2019-06-19 17:11:24 -0400

    added readme

--- routes/routes.js
+++ routes/routes.js
@@ -2,39 +2,41 @@ var nodeMailer = require('nodemailer');
 
 module.exports = function(app) {
 
-app.get('/', function (req, res) {
-    res.render('index');
-});
+    app.get('/', function (req, res) {
+        res.render('index');
+    });
 
-app.post('/send-email', function (req,res) {
-  console.log(req.body)
-  var smtpConfig = {
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, 
-    auth: {
-        user: 'adam.ravitz.portfolio@gmail.com',
-        pass: 'R@v1tz87'
-    }
-  };
-var transporter = nodeMailer.createTransport(smtpConfig);
+    //route that send an email via nodemailer to my email address from my portfolios email address
+    app.post('/send-email', function (req,res) {
 
-var mailOptions = {
-    from: 'adam.ravitz.portfolio@gmail.com',
-    to: 'adam.ravitz@gmail.com',
-    subject:  `a message from your portfolio from ${req.body.name}`,
-    text: `${req.body.message} \nyou can respond to them at ${req.body.email}`
-};
+        //specific configuration for gmail 
+        var smtpConfig = {
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, 
+            auth: {
+                user: 'adam.ravitz.portfolio@gmail.com',
+                pass: 'R@v1tz87'
+            }
+    };
 
-transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-        // res.redirect('/');
+    var transporter = nodeMailer.createTransport(smtpConfig);
 
-    }
-});
-})
+    var mailOptions = {
+        from: 'adam.ravitz.portfolio@gmail.com',
+        to: 'adam.ravitz@gmail.com',
+        subject:  `a message from your portfolio from ${req.body.name}`,
+        text: `${req.body.message} \nyou can respond to them at ${req.body.email}`
+        };
 
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            // res.redirect('/');
+
+        }
+    });
+    })
 }
